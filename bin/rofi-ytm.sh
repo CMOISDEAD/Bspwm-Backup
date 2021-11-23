5e92ff0d53a1f0a341712fc10b3c251e61c6319f
camilo davila <camilodavina@gmail.com>
2021-11-23 18:58:43 -0500

    First Commit

---
+++ bin/rofi-ytm.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+rofi -theme ~/Documents/github/rofi-ytm/menu.rasi -modi blocks -show blocks -blocks-wrap /home/camilo/Documents/github/rofi-ytm/rofi-ytm.py
---
+++ bin/undercovermode.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+killall polybar 
+ 
+/home/camilo/.config/openbox/polybar/eleven/launch.sh
+
+feh --bg-fill /usr/share/backgrounds/eleven-dark.jpg
